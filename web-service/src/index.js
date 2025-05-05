5a6f8c6317e7fb31876cb801ce3a763c5273a00b
PauloPaulMonayer <monayer42@gmail.com>
2025-05-05 20:26:09 +0300

    feat(redis): integrate Redis hits counter

--- web-service/src/index.js
+++ web-service/src/index.js
@@ -1,13 +1,34 @@
 const express = require("express");
+const { createClient } = require("redis");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from DevOps project!" });
+const redisClient = createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
 });
 
+redisClient.on("error", (err) => console.error("Redis Client Error", err));
+
+(async () => {
+  await redisClient.connect();
+  console.log("Connected to Redis");
+})();
+
+app.get("/", async (req, res) => {
+  try {
+    const hits = await redisClient.incr("hits");
+    res.json({
+      message: "Hello from Paulo and Guy!",
+      hits,
+    });
+  } catch (err) {
+    console.error("Redis error", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// 5. הפעלת השרת
 app.listen(PORT, () => {
   console.log(`Web service listening on http://localhost:${PORT}`);
 });
